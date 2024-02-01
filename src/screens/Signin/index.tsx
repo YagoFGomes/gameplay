bfe3e987c5da8e157eb9066969d3403d9ed9aa8b
Yago Ferreira <yago.fgomes@gmail.com>
2024-02-01 12:50:54 +0000

    finalização da screen de login

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,10 +2,16 @@ import { Text, View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
 import Illustration from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 export function Signin() {
     return (
         <View style={styles.container}>
+            <StatusBar
+                style="light"
+                backgroundColor="transparent"
+                translucent
+            />
             <Image
                 source={Illustration}
                 style={styles.image}
@@ -21,6 +27,7 @@ export function Signin() {
                     Crie grupos para jogar seus games {`\n`}
                     favoritos com seus amigos
                 </Text>
+                <ButtonIcon title="Entrar com discord" activeOpacity={0.7} />
             </View>
         </View>
     );
